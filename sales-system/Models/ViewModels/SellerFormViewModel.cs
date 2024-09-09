1231f5523312e991539c0facd85c49c73e2f234f
Dudev-fy <santos.eduardom06.03@gmail.com>
2024-09-09 00:11:05 -0300

    SellerFormViewModel and Department select component

---
+++ sales-system/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace sales_system.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
---
+++ sales-system/Services/DepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using sales_system.Data;
+using sales_system.Models;
+
+namespace sales_system.Services
+{
+    public class DepartmentService
+    {
+        private readonly sales_systemContext _context;
+
+        public DepartmentService(sales_systemContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
